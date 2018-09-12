2bd97da895af00b8a59c217b79a086b2dd24868e
Gabriella Catalina Costilla <gabycostilla@swuser-129-8-233-234.swlan.csufresno.edu>
2018-09-11 17:20:43 -0700

    I added the first method

--- main.ts
+++ main.ts
@@ -10,11 +10,15 @@ class MyFirstProgram {
     }
 
     runFirst(callback:Function){
-        console.log('this method runs first...');
+        console.log('this method runs first...!');
     }
 
     runLast(){
-        console.log('this method runs last');
+        console.log('this method runs last!!');
+    }
+
+    runMiddle(){
+        console.log('this code run in between the other two functions ...');
     }
 }
 
